0b6b4e9d6c7360be4bf828a2fdb0431517c656dc
shadowbq <shadowbq@gmail.com>
2015-05-15 11:36:20 -0400

    fixing tests

--- test/test_waldo.rb
+++ test/test_waldo.rb
@@ -1,8 +1,9 @@
+require 'test_helper'
 require 'minitest/spec'
 
-describe Where do
+describe Barnyard2::Waldo::Where do
   before do
-    @where = finder = Barnyard2::Waldo::Where.new('test/data/barnyard.waldo')
+    @where = Barnyard2::Waldo::Where.new('test/data/barnyard.waldo')
   end
 
   describe "when looking for waldo" do
@@ -22,7 +23,7 @@ describe Where do
 
   describe "when asking to print" do
     it "it should print the string" do
-      @where.to_s.must_equal ''
+      @where.to_s.must_equal "Barnyard spool: /var/log/snort/merged.log.1426175522\nRecord Idx: 119879\n2015-03-12 11:52:02 -0400"
     end
   end
 end
